1c0211ac91aabe794805a936a2928113b154761e
1 <121359002+Gauravkumar009@users.noreply.github.com>
2025-08-06 16:58:50 +0000

    ob

---
+++ Object/object_02.js
@@ -0,0 +1,21 @@
+//const tinderUser = new Object()
+
+const tinderUser = {}
+
+tinderUser.id ="1213"
+tinderUser.name = "Aman Shakya"
+tinderUser.isloggedIn= false
+
+// console.log(tinderUser)
+
+const regularUser = {
+    email : "gauravv8734@gmail.com",
+    fullname :{
+        userfullname : {
+            firstname : "Aman",
+            lastname : "Shakay"
+        }
+    }
+}
+
+console.log(regularUser.fullname)
