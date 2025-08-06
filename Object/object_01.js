0d620b23f7142e30180ac7780753948a94b81e4c
1 <121359002+Gauravkumar009@users.noreply.github.com>
2025-08-06 16:22:48 +0000

    object

--- Object/object_01.js
+++ Object/object_01.js
@@ -1,8 +1,34 @@
+const mySym = "Aman Shakya";
+
 const jsUser = {
     name:"Aman",
+    "full name": "Gaurav kumar",
+    [mySym] : "mykey1",
     age : 21,
     location:"Greater Noida",
-    [mySym]:"mykey1"
+    email :"gauravv8745@gmail.com",
+    isloggedIn : false,
+    lastloggedinDays:["Monday","Saturday"]
 }
 
-console.log(jsUser.name);
+// console.log(jsUser.emai)
+// console.log(jsUser["email"]) 
+// console.log(jsUser["full name"]) 
+// console.log(jsUser[mySym]) 
+
+jsUser.email = "gauravv8745@gmail.com"
+// Object.freeze(jsUser)
+jsUser.email = "gauravv8745@gmail.com"
+
+// console.log(jsUser)
+
+jsUser.greeting = function(){
+    console.log("Hello JS User");
+}
+
+jsUser.greetingTwo = function(){
+    console.log(`Hello JS User, ${this.name}`);
+}
+
+console.log(jsUser.greeting());
+console.log(jsUser.greetingTwo());
