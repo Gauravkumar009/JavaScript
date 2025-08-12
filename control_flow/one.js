e414479fd79d767274fe90e8f1bf237c26f3116f
1 <121359002+Gauravkumar009@users.noreply.github.com>
2025-08-12 17:36:51 +0000

    control

---
+++ control_flow/one.js
@@ -0,0 +1,32 @@
+// if 
+// const temperature =41;
+// if (temperature === 41) {
+//     console.log("temperature is less than 50");
+// }else{
+//     console.log("temperature is greater than 50");
+// }
+
+// console.log("Execute");
+// const score = 200;
+// if (score >100) {
+//     const power = "Fly"
+//     console.log(`User power: ${power}`);
+// }
+
+
+// const coding =["js","ruby","java",'python','cpp']
+
+// const as = coding.forEach((item) =>{
+//     console.log(item);
+
+// });
+
+// console.log(as);
+
+const num = [1,2,3,4,5,6,7,8,9,10]
+
+// const nums = num.filter((num1) => num1>4)
+// console.log(nums);
+
+const as = num.map((n)=>{return n+10})
+console.log(as);
