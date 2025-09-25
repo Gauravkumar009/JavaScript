3c8627f1a6551b9ff71698f863f00c9357eac3f2
1 <121359002+Gauravkumar009@users.noreply.github.com>
2025-09-25 07:14:28 +0000

    opps

---
+++ Classes_and_oops/Prototype.js
@@ -0,0 +1,17 @@
+let myHeros = ["thor", "spiderman"]
+
+
+let heroPower = {
+    thor: "hammer",
+    spiderman: "sling",
+
+    getSpiderPower: function(){
+        console.log(`Spidy power is ${this.spiderman}`);
+    }
+}
+
+Object.prototype.aman = function(){
+    console.log(`aman is present in all oblects`);
+}
+
+myHeros.aman()
